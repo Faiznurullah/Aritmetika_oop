083973802b352609404216a19780dd79d79b5446
Faiz nurullah <59213454+Faiznurullah@users.noreply.github.com>
2021-01-05 05:10:14 +0700

    Add files via upload

---
+++ aritmetika_oop/model.php
@@ -0,0 +1,84 @@
+<?php
+
+ class mtk{
+
+//method1
+   public function penjumlahan(){
+
+     if(isset($_POST['submit'])){
+       $angka1 = htmlspecialchars($_POST["1"]);
+       $angka2 = htmlspecialchars($_POST["2"]);
+       $aksi = $angka1 + $angka2;
+
+  echo "$angka1 + $angka2 = $aksi";
+
+}else{
+  echo "Silahkan Mengoprasikan";
+}
+
+
+   }
+
+//method2
+   public function pengurangan(){
+
+     if(isset($_POST['submit'])){
+       $angka1 = htmlspecialchars($_POST["1"]);
+       $angka2 = htmlspecialchars($_POST["2"]);
+       $aksi = $angka1 - $angka2;
+
+  echo "$angka1 - $angka2 = $aksi";
+
+}else{
+  echo "Silahkan Mengoprasikan";
+}
+
+
+   }
+
+
+//method3
+   public function perkalian(){
+
+     if(isset($_POST['submit'])){
+       $angka1 = htmlspecialchars($_POST["1"]);
+       $angka2 = htmlspecialchars($_POST["2"]);
+       $aksi = $angka1 * $angka2;
+
+  echo "$angka1 x $angka2 = $aksi";
+
+}else{
+  echo "Silahkan Mengoprasikan";
+}
+
+
+   }
+
+
+   //method4
+   public function pembagian(){
+
+     if(isset($_POST['submit'])){
+       $angka1 = htmlspecialchars($_POST["1"]);
+       $angka2 = htmlspecialchars($_POST["2"]);
+       $aksi = $angka1 / $angka2;
+
+  echo "$angka1 : $angka2 = $aksi";
+
+}else{
+  echo "Silahkan Mengoprasikan";
+}
+
+
+   }
+
+
+ }
+
+$mtk = new mtk;
+
+//
+
+
+echo "Penampung Method";
+?>
---
+++ aritmetika_oop/penjumlahan.php
@@ -0,0 +1,26 @@
+<?php
+include 'model.php';
+ ?>
+<!doctype html>
+ <html lang="id">
+ <head>
+<title>Input OOP</title>
+ </head>
+   <body>
+
+<form  method="post" enctype="multipart/form-data">
+     <label>Angka Pertama</label>
+     <input type="number" name= '1' placeholder="angka...">
+  
+     <label>Angka Kedua</label>
+     <input type="number" name= '2' placeholder="angka...">
+     <input type="submit" name="submit" value="kirim">
+
+</form>
+<?php
+$penjumlahan = $mtk->penjumlahan();
+?>
+
+
+  </body>
+ </html>
